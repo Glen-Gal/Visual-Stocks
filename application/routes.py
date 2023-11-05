2bbc7e39a79337c4da29fade13e4cf3d874427f1
Jamphel <Jamphelyigzins15@gmail.com>
2023-11-05 22:00:53 +0600

    first commit

---
+++ application/routes.py
@@ -0,0 +1,11 @@
+from application import app
+from flask import render_template, url_for
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+@app.route("/")
+def index():
+    from application import app 
+    return render_template("layout.html")
